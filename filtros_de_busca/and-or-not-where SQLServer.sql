8fa19894204333fbde1c429e0824795132f6e7e2
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-07-25 02:00:10 -0300

    Update and-or-not-where SQLServer.sql

--- filtros_de_busca/and-or-not-where SQLServer.sql
+++ filtros_de_busca/and-or-not-where SQLServer.sql
@@ -61,3 +61,38 @@ select
 from
 	DimEmployee
 where not DepartmentName = 'Marketing'
+
+--EXERCICIOS:
+
+select * from DimEmployee
+where Gender = 'F' and DepartmentName = 'Finance'
+
+select * from DimProduct
+where UnitPrice >= 100 and BrandName = 'Contoso' and ColorName = 'Red'
+
+select * from DimProduct
+where BrandName = 'Litware' or BrandName = 'Fabrikam' OR ColorName = 'Black'
+
+select * from DimSalesTerritory
+where SalesTerritoryGroup = 'Europe' and not SalesTerritoryCountry = 'Italy'
+
+select * from DimProduct
+where BrandName = 'Fabrikam' and (ColorName = 'Black' or ColorName = 'Red')
+
+
+-- IN = entre
+
+--IN:
+
+select * from DimProduct
+where ColorName in ('Silver','Blue', 'White', 'Red', 'Black')
+
+-- LIKE = tem isso em tal? nome_completo LIKE '%Felipe%', pega todos nomes que tem felipe em algum lugar
+
+-- LIKE:
+select * from DimProduct
+where ProductName like '%MP3 Player%'
+
+select * from DimProduct
+where ProductDescription like '%Type%'  -- %''% ENTRE % pode esta em qualquer lugar do texto, ''%: apenas no final, %'': apenas no começo
+
