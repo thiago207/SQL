22ad785692bd85fe2aad2f9e90ab3d05e97ee05b
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-07-29 21:13:58 -0300

    group by sql server

--- agregacao_agrupamento/AGRUPAMENTOS ( group by ) NO SQLServer.sql
+++ agregacao_agrupamento/AGRUPAMENTOS ( group by ) NO SQLServer.sql
@@ -161,52 +161,131 @@ a) Você deve fazer uma consulta à tabela FactOnlineSales e descobrir qual é o ID
 SalesQuantity).*/
 
 
-select * from fav
+select top(10) * from FactOnlineSales
+
+select top(1)
+	CustomerKey, 
+	sum(SalesQuantity) 
+from 
+	FactOnlineSales
+group by CustomerKey
+order by SUM(SalesQuantity) desc
 
 /*
 b) Feito isso, faça um agrupamento de total vendido (SalesQuantity) por ID do produto
 e descubra quais foram os top 3 produtos mais comprados pelo cliente da letra a).*/
 
+select top(3)
+	sum(SalesQuantity) as 'total comprado',
+	ProductKey as 'ID Produtos mais comprados'
+from 
+	FactOnlineSales
+where CustomerKey = 19037
+group by ProductKey
+order by [total comprado] desc
 
 /*
 DIMPRODUCT
 4. a) Faça um agrupamento e descubra a quantidade total de produtos por marca.*/
 
+select * from DimProduct
+
+select BrandName, count(*) as 'quantidade total' from DimProduct
+group by BrandName
+
 /*
 b) Determine a média do preço unitário (UnitPrice) para cada ClassName.*/
 
+select ClassName, avg(UnitPrice) as 'media preco' from DimProduct
+group by ClassName
+order by [media preco] desc
+
 /*
 c) Faça um agrupamento de cores e descubra o peso total que cada cor de produto possui.*/
+
+select ColorName, sum(Weight) as 'peso total por cor' from DimProduct
+group by ColorName
+order by [peso total por cor] desc
+
 
 /*
 5. Você deverá descobrir o peso total para cada tipo de produto (StockTypeName).
 A tabela final deve considerar apenas a marca ‘Contoso’ e ter os seus valores classificados em
 ordem decrescente.*/
 
+select 
+	StockTypeName, 
+	sum(Weight) 
+from 
+	DimProduct
+where BrandName = 'Contoso'
+group by StockTypeName
+order by sum(Weight) desc
+
+
 /*
 6. Você seria capaz de confirmar se todas as marcas dos produtos possuem à disposição todas as
 16 opções de cores?*/
 
+select 
+	BrandName, count(distinct ColorID) as 'qtd_cores'
+from 
+	DimProduct
+group by BrandName
+HAVING COUNT(DISTINCT ColorID) < 16
 
---	DIMCUSTOMER
+select distinct ColorName from DimProduct
+where BrandName = 'Proseware'     -- saber quais sao da que menos tem 
+
+--	DIMCUSTOMER	
 
 /*
 7. Faça um agrupamento para saber o total de clientes de acordo com o Sexo e também a média
 salarial de acordo com o Sexo. Corrija qualquer resultado “inesperado” com os seus
 conhecimentos em SQL.*/
 
+select * from DimCustomer
+
+select Gender, count(Gender) as 'qtd. por sexo', avg(YearlyIncome)  as 'media salaria' from DimCustomer
+where Gender is not null
+group by Gender
+
 /*
 8. Faça um agrupamento para descobrir a quantidade total de clientes e a média salarial de
 acordo com o seu nível escolar. Utilize a coluna Education da tabela DimCustomer para fazer
 esse agrupamento.*/
+
+select Education ,count(*) as 'qtd total de clientes', avg(YearlyIncome) from DimCustomer
+where Education is not null
+group by Education
+
 
 /*
 DIMEMPLOYEE
 9. Faça uma tabela resumo mostrando a quantidade total de funcionários de acordo com o
 Departamento (DepartmentName). Importante: Você deverá considerar apenas os
 funcionários ativos.*/
+select * from DimEmployee
+
+select 
+	DepartmentName, count(*) as 'qtd total de funcionarios' 
+from 
+	DIMEMPLOYEE
+where Status = 'Current'
+group by DepartmentName
+order by [qtd total de funcionarios] desc
 
 /*
 10. Faça uma tabela resumo mostrando o total de VacationHours para cada cargo (Title). Você
 deve considerar apenas as mulheres, dos departamentos de Production, Marketing,
 Engineering e Finance, para os funcionários contratados entre os anos de 1999 e 2000.*/
+
+select 
+	Title , 
+	sum(VacationHours)  as 'Horas Totais' 
+from 
+	DimEmployee
+where Gender = 'F' and 
+	DepartmentName in ('Production', 'Marketing', 'Engineering', 'Finance') 
+	and HireDate between '1999-01-01' and '2000-12-31'
+group by Title
