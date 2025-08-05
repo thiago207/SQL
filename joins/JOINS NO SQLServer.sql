792bd76788729187bd914848f76c61fac2d820f1
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-05 16:50:53 -0300

    Update JOINS NO SQLServer.sql

--- joins/JOINS NO SQLServer.sql
+++ joins/JOINS NO SQLServer.sql
@@ -1,22 +1,17 @@
 -- JOINS
 
 
-select * from DimChannel
-select top(100) * from FactSales
-
-select top(100) 
-	c.ChannelDescription, 
-	avg(f.UnitCost) 
-from 
-	DimChannel as c
-LEFT JOIN FactSales as f
-ON c.ChannelKey = f.channelKey
-group by c.ChannelDescription 
-
-
 --LEFT JOIN
+select * from produtos
+select * from subcategoria
 
-
+select 
+	p.id_produto, p.nome_produto, p.id_subcategoria,
+	s.nome_subcategoria
+from 
+	produtos as p
+LEFT JOIN subcategoria as s
+ON p.id_subcategoria = s.id_subcategoria
 
 --RIGHT JOIN
 
