f7813389d3be699120e41905a1813a77b29d8063
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-04 22:09:35 -0300

    Update JOINS NO SQLServer.sql

--- joins/JOINS NO SQLServer.sql
+++ joins/JOINS NO SQLServer.sql
@@ -1,3 +1,12 @@
 -- JOINS
 
 
+select * from DimChannel
+
+select top(100) * from FactSales
+
+select top(100) c.ChannelDescription, avg(f.UnitCost) 
+from DimChannel as c
+LEFT JOIN FactSales as f
+ON c.ChannelKey = f.channelKey
+group by c.ChannelDescription
