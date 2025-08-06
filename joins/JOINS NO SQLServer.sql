b076d1ac13aa4d07f36e7a42d84ea4a52896995a
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-05 21:37:03 -0300

    Update JOINS NO SQLServer.sql

--- joins/JOINS NO SQLServer.sql
+++ joins/JOINS NO SQLServer.sql
@@ -59,13 +59,57 @@ select
 	s.nome_subcategoria
 from 
 	produtos as p
-LEFT ANTI JOIN subcategoria as s
+LEFT JOIN subcategoria as s
 ON p.id_subcategoria = s.id_subcategoria
-
+where nome_subcategoria is null
 
 
 --RIGHT ANTI JOIN
 
+select 
+	p.id_produto, p.nome_produto, p.id_subcategoria,
+	s.nome_subcategoria
+from 
+	produtos as p
+right JOIN subcategoria as s
+ON p.id_subcategoria = s.id_subcategoria
+where id_produto is null
 
 
 --FULL ANTI JOIN
+
+select	
+	p.id_produto, p.nome_produto, p.id_subcategoria,
+	s.nome_subcategoria
+from 
+	produtos as p
+full JOIN subcategoria as s
+ON p.id_subcategoria = s.id_subcategoria
+where id_produto  is  null or s.id_subcategoria is null
+
+
+--EXEMPLOS:
+
+select ProductKey, ProductName, ProductSubcategoryKey from DimProduct
+select ProductSubcategoryKey, ProductSubcategoryName from DimProductSubcategory
+
+select 
+	p.ProductKey, p.ProductName, p.ProductSubcategoryKey,
+	s.ProductSubcategoryName
+
+from DimProduct as p
+right join DimProductSubcategory  as s
+on p.ProductSubcategoryKey = s.ProductSubcategoryKey
+
+--COMO ESCOLHER ENTRE RIGHT OU LEFT JOIN?
+
+--1. A LEFT é para complementar informacao da tabelas do produtos (LEFT) de acordo com a tabela de categoria (RIGHT)
+select 
+	p.id_produto, p.nome_produto, p.id_subcategoria,
+	s.nome_subcategoria
+from 
+	produtos as p
+FULL JOIN subcategoria as s
+ON p.id_subcategoria = s.id_subcategoria
+
+
