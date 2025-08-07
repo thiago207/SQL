e98dd6321f55128f113d72dae5ff16c08237a9f0
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-06 21:39:27 -0300

    Update EXERCICIOS JOINS SQLServer.sql

--- joins/EXERCICIOS JOINS SQLServer.sql
+++ joins/EXERCICIOS JOINS SQLServer.sql
@@ -81,7 +81,7 @@ select top(100) * from DimAccount
 • AccountName
 • Amount*/ 
 
-select s.StrategyPlanKey,
+select top (100) s.StrategyPlanKey,
 	s.DateKey,
 	s.Amount,
 	a.AccountName
@@ -89,3 +89,24 @@ from FactStrategyPlan as s
 inner join DimAccount as a
 on s.AccountKey = a.AccountKey
 
+--6)
+--Vamos continuar analisando a tabela FactStrategyPlan. Além da coluna AccountKey que
+--identifica o tipo de conta, há também uma outra coluna chamada ScenarioKey. Essa coluna
+--possui a numeração que identifica o tipo de cenário: Real, Orçado e Previsão.
+--Faça um INNER JOIN para criar uma tabela contendo o ScenarioName para cada ScenarioKey
+--da tabela FactStrategyPlan. O seu SELECT final deve conter as colunas:
+
+/* StrategyPlanKey
+ DateKey
+ ScenarioName
+ Amount*/
+select top(100) * from FactStrategyPlan
+
+
+select top (100) s.StrategyPlanKey,
+	s.DateKey,
+	s.Amount,
+	a.AccountName
+from FactStrategyPlan as s
+inner join DimAccount as a
+on s.AccountKey = a.AccountKey
