c34a5ceb4d5d9b3dcd62d2ad16875d746c9e628d
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-06 21:06:07 -0300

    Create EXERCICIOS JOINS SQLServer.sql

---
+++ joins/EXERCICIOS JOINS SQLServer.sql
@@ -0,0 +1,26 @@
+--EXERCICIOS JOINS:
+
+--1)
+
+--Utilize o INNER JOIN para trazer os nomes das subcategorias dos produtos, da tabela
+--DimProductSubcategory para a tabela DimProduct.
+
+select * from DimProduct
+select * from DimProductSubcategory
+
+
+select p.*,
+	   s.ProductSubcategoryName
+from DimProduct as p
+inner join DimProductSubcategory as s
+on p.ProductSubcategoryKey = s.ProductSubcategoryKey
+
+
+--2)
+--Identifique uma coluna em comum entre as tabelas DimProductSubcategory e
+--DimProductCategory. Utilize essa coluna para complementar informações na tabela
+--DimProductSubcategory a partir da DimProductCategory. Utilize o LEFT JOIN.
+select * from DimProductSubcategory
+select * from DimProductSubcategory
+
+
