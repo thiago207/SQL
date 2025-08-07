ad3ade5a59c32b11873b3d99177876e2f74dbc80
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-06 21:10:05 -0300

    Update EXERCICIOS JOINS SQLServer.sql

--- joins/EXERCICIOS JOINS SQLServer.sql
+++ joins/EXERCICIOS JOINS SQLServer.sql
@@ -20,7 +20,13 @@ on p.ProductSubcategoryKey = s.ProductSubcategoryKey
 --Identifique uma coluna em comum entre as tabelas DimProductSubcategory e
 --DimProductCategory. Utilize essa coluna para complementar informações na tabela
 --DimProductSubcategory a partir da DimProductCategory. Utilize o LEFT JOIN.
-select * from DimProductSubcategory
+select * from DimProductCategory
 select * from DimProductSubcategory
 
+select s.*,
+	   c.ProductSubcategoryName
+FROM DimProductSubcategory as s
+left join DimProductCategory as c
+on s.ProductCategoryDescription = c.ProductCategoryDescription
+	
 
