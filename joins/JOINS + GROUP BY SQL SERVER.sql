e1382cb5e4b69b5463ba4f98c6b623134271e4e5
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-20 20:56:27 -0300

    Update JOINS + GROUP BY SQL SERVER.sql

--- joins/JOINS + GROUP BY SQL SERVER.sql
+++ joins/JOINS + GROUP BY SQL SERVER.sql
@@ -34,7 +34,32 @@ group by d.CalendarYear
 HAVING sum(SalesQuantity) >= 1200000
 
 
+--Os exercícios abaixo estão divididos de acordo com uma determinada tabela do Banco de Dados. Você pode utilizar o INNER JOIN para resolver todas as questões.
 
+--FACT SALES
+--1. a) Faça um resumo da quantidade vendida (Sales Quantity) de acordo com o nome do canal
+--de vendas (ChannelName). Você deve ordenar a tabela final de acordo com SalesQuantity,
+--em ordem decrescente.
+
+SELECT 
+	c.ChannelName,
+	sum(SalesQuantity)
+from
+	FactSales as f
+inner join DimChannel as c
+on f.channelKey = c.ChannelKey
+group by c.ChannelName
+
+
+--b) Faça um agrupamento mostrando a quantidade total vendida (Sales Quantity) e
+--quantidade total devolvida (Return Quantity) de acordo com o nome das lojas
+--(StoreName).
+
+
+
+
+--c) Faça um resumo do valor total vendido (Sales Amount) para cada mês
+--(CalendarMonthLabel) e ano (CalendarYear)
 
 
 
