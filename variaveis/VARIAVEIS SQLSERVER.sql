1b24feadbe781d26e27b01fc50f0ddc3a3c8319d
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-27 22:22:43 -0300

    Update VARIAVEIS SQLSERVER.sql

--- variaveis/VARIAVEIS SQLSERVER.sql
+++ variaveis/VARIAVEIS SQLSERVER.sql
@@ -1,1 +1,45 @@
---Variaveis 
+﻿--Variaveis 
+-- [SQL Server] Vari�veis
+-- Aula 2 de 23: Tipos de Dados
+
+/* 
+Tipos de dados
+
+O tipo de dado � a maneira como o SQL consegue diferenciar cada valor dentro de um banco de dados.
+
+a) Inteiro 
+Exemplos:
+1, 100, 569
+
+Como o SQL entende um numero inteiro: INT
+
+
+b) Decimal
+Exemplos:
+10.33, 90.91, 410.787
+
+Como o SQL entende um n�mero decimal: FLOAT
+Como o SQL entende um n�mero decimal: DECIMAL(N, M)
+
+N � a quantidade de d�gitos que o n�meto pode ter, incluindo casas decimais
+M � o n�mero m�ximo de casas decimais
+
+
+c) Texto/String
+Exemplos:
+'Carla', 'Motorola', 'Pastel', '44'
+
+Como o SQL entende um texto: VARCHAR(N)
+
+N � o n�mero de caracteres que o texto pode ter
+
+
+d) Data
+Exemplos:
+'01/01/2021', '23/03/2021'
+
+Como o SQL entende uma data/hora: DATETIME
+Como o SQL entende uma data: DATE
+
+*/
+
