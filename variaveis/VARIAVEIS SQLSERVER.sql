55de0c47bbb157e812ba178157500a4dcbbaae1f
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-27 22:31:29 -0300

    Update VARIAVEIS SQLSERVER.sql

--- variaveis/VARIAVEIS SQLSERVER.sql
+++ variaveis/VARIAVEIS SQLSERVER.sql
@@ -18,8 +18,8 @@ b) Decimal
 Exemplos:
 10.33, 90.91, 410.787
 
-Como o SQL entende um n�mero decimal: FLOAT
-Como o SQL entende um n�mero decimal: DECIMAL(N, M)
+Como o SQL entende um numero decimal: FLOAT
+Como o SQL entende um numero decimal: DECIMAL(N, M)		
 
 N � a quantidade de d�gitos que o n�meto pode ter, incluindo casas decimais
 M � o n�mero m�ximo de casas decimais
@@ -42,4 +42,26 @@ Como o SQL entende uma data/hora: DATETIME
 Como o SQL entende uma data: DATE
 
 */
+
+--Operacoes basicas:
+
+Select 10 as 'Numero:'
+select 'Thiago' as 'nome'
+select '20/07/2007' as 'data'
+
+
+--Operacoes com numeros
+
+select 10+20 as 'soma'
+select 20-10 as 'subtracao'
+select 10 * 10 'mutiplicacao'
+select 181.0/5 as 'divisao'  --(.0) INTEIRO
+
+--Operacao texto
+
+select 'Thiago' + ' ' + 'Felipe'
+
+--Operacoes com data
+
+select 20/07/2007 + 1
 
