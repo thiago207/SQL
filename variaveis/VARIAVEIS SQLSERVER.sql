d44031e048cbcfb7d2ab5cf0c82ed88822a7b3d3
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-09-02 20:24:16 -0300

    Update VARIAVEIS SQLSERVER.sql

--- variaveis/VARIAVEIS SQLSERVER.sql
+++ variaveis/VARIAVEIS SQLSERVER.sql
@@ -227,3 +227,30 @@ select
 	UnitPrice - (UnitPrice * @desconto) as 'preco com desconto'
 from 
 	DimProduct
+
+
+-- Crie uma variável de data para otimizar a consulta abaixo. 
+
+DECLARE @varData DATETIME
+SET @varData = '01/01/1980'
+
+SELECT 
+	FirstName AS 'Nome',
+	LastName AS 'Sobrenome',
+	BirthDate AS 'Nascimento',
+	'Cliente' AS 'Tipo'
+FROM	
+	DimCustomer
+WHERE BirthDate >= @varData
+
+UNION
+
+SELECT 
+	FirstName AS 'Nome',
+	LastName AS 'Sobrenome',
+	BirthDate AS 'Nascimento',
+	'Funcionário' AS 'Tipo'
+FROM	
+	DimEmployee
+WHERE BirthDate >= @varData
+ORDER BY Nascimento
