c5210e8d61e75d41970a0b5e01f75ea815ccdeed
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-10-07 21:26:32 -0300

    Create NUMERO TEXTO DATA SQL SERVER.sql

---
+++ numero_texto_data/NUMERO TEXTO DATA SQL SERVER.sql
@@ -0,0 +1,22 @@
+--NUMERO TEXTO E DATA NO SQL SERVER
+
+
+--LEN e DATALENGTH
+-- Exemplo: Descubra a quantidade de caracteres da palavra 'SQL Hashtag'
+
+SELECT
+	LEN('SQL Hashtag') AS 'Len',
+	DATALENGTH('SQL Hashtag') AS 'Datalength'
+
+
+
+-- CONCAT ---> Permite juntar mais de um texto em uma única palavra
+-- Exemplo: Faça uma consulta à tabela DimCustomer onde seja possível visualizar o nome completo de cada cliente.
+
+SELECT
+	FirstName AS 'Nome',
+	LastName AS 'Sobrenome',
+	EmailAddress AS 'E-mail',
+	CONCAT(FirstName, ' ', LastName) AS 'Nome Completo'
+FROM
+	DimCustomer
