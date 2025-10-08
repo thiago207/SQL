e51e544d8a9e1a1453b59a315fe2ec40288eaa65
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-10-07 21:37:01 -0300

    Update NUMERO TEXTO DATA SQL SERVER.sql

--- numero_texto_data/NUMERO TEXTO DATA SQL SERVER.sql
+++ numero_texto_data/NUMERO TEXTO DATA SQL SERVER.sql
@@ -23,3 +23,16 @@ FROM
 
 
 
+-- LEFT ---> Extrai uma determinada quantidade de caracteres de um texto, da esquerda para a direita
+-- RIGHT ---> Extrai uma determinada quantidade de caracteres de um texto, da direita para a esquerda
+-- Exemplo: Faça uma consulta à tabela DimProduct e divida a coluna de StyleName em 2 partes
+
+SELECT * FROM DimProduct
+SELECT
+	ProductName AS 'Produto',
+	UnitPrice AS 'Preço',
+	LEFT(StyleName, 7) AS 'Cod 1',
+	RIGHT(StyleName, 7) AS 'Cod 2'
+FROM
+	DimProduct
+
